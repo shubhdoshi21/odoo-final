af1f65cb6412acb48d2f17bdbb4415a968e5111a
Priyal208 <priyalrwl.20@gmail.com>
2024-07-14 08:10:01 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
           <TopBar />
-          {/* <Footer/> */}
+          <Footer/> 
         </div>
       </Router>
     </>
